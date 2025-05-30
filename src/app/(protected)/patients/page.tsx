bd1e0ccfedd35e735cf611d8408bcd65018ebfd2
Fcaires <fc.caires@hotmail.com>
2025-05-29 21:07:14 -0300

    feat: ensure patients can be created and updated

---
+++ src/app/(protected)/patients/page.tsx
@@ -0,0 +1,34 @@
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
+import AddPatientButton from "./_components/add-patients-button";
+
+const PatientsPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Pacientes</PageTitle>
+          <PageDescription>
+            Gerencie os pacientes da sua clínica
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <AddPatientButton />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <div className="*: grid grid-cols-3 gap-6"></div>
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default PatientsPage;
